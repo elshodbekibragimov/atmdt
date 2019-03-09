7047bc49295071dddb66143b3caf9f65aa42b878
Elshod <elshodbek_999622@mail.ru>
2019-03-10 00:18:49 +0500

    elshodbel

---
+++ temp4/application.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/lay")
+def layout():
+    return render_template("layout.html")
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
